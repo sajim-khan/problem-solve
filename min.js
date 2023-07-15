d76df45fe7dfe6de470c96f1a89131d2bc97d839
sajim-khan <shossain1427@gmail.com>
2023-07-15 23:57:00 +0600

    added min num from an array

--- min.js
+++ min.js
@@ -2,7 +2,7 @@ var arr = [3, 6, 2, 56, 32, 5, 89, 325];
 var largest = arr[0];
 
 for (var i = 0; i < arr.length; i++) {
-  if (arr[i] > largest) {
+  if (arr[i] < largest) {
     largest = arr[i];
   }
 }
