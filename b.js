f57a72663b0fc37050252b311d400a1bbc30611b
sajim-khan <shossain1427@gmail.com>
2023-07-22 20:54:55 +0600

    added callback function

--- b.js
+++ b.js
@@ -1,1 +1,12 @@
+// Explain what a callback function is and provide a simple example?
 
+function greet(name, callback) {
+  console.log("Hello, " + name + "!");
+  callback();
+}
+
+function sayGoodbye() {
+  console.log("Goodbye!");
+}
+
+greet("Alice", sayGoodbye);
