8ee3f7eae43e18e6e2c7cd835f54960be7f6c294
sajim-khan <shossain1427@gmail.com>
2023-07-22 20:25:21 +0600

    added a code to calculate if the string is palindrome

--- c.js
+++ c.js
@@ -1,1 +1,8 @@
+//Write a code to calculate if the string is palindrome ? (Use javascript only)
 
+function isPalindrome(str) {
+  const formattedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  return formattedStr === formattedStr.split('').reverse().join('');
+}
+
+console.log(isPalindrome("level"))//true
