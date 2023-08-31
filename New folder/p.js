c77af16bb3ac24eb9bb5afb6cbfad9e5140b1e7c
sajim-khan <shossain1427@gmail.com>
2023-08-31 22:43:28 +0600

    added string concat

---
+++ New folder/p.js
@@ -0,0 +1,9 @@
+function concatenateStrings(str1, str2) {
+  return str1 + str2;
+}
+
+// Test the function
+const string1 = "Hello, ";
+const string2 = "World!";
+const concatenatedString = concatenateStrings(string1, string2);
+console.log(concatenatedString); // Output: "Hello, World!"
