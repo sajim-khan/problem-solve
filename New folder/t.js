4cb05783bb3771f52f08f5e26ffe54641aabf52b
sajim-khan <shossain1427@gmail.com>
2023-09-08 19:45:12 +0600

    solved a problem that max num from an array

---
+++ New folder/t.js
@@ -0,0 +1,20 @@
+function findMaxNumber(arr) {
+    if (arr.length === 0) {
+        return null; // Handle an empty array
+    }
+    
+    let max = arr[0];
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    
+    return max;
+}
+
+// Test the function
+const numbers = [12, 45, 6, 89, 23, 1, 67];
+const maxNumber = findMaxNumber(numbers);
+console.log(maxNumber);  // Output: 89
