1008effc23d4e001400d11cde79f5e5123cd4ce7
sajim-khan <shossain1427@gmail.com>
2023-09-08 21:25:02 +0600

    added Find the Sum of All negative Numbers in an Array

---
+++ New folder/x.js
@@ -0,0 +1,16 @@
+function sumOfNegativeNumbers(arr) {
+  let sum = 0;
+
+  for (const num of arr) {
+    if (num < 0) {
+      sum += num;
+    }
+  }
+
+  return sum;
+}
+
+// Test the function
+const numbers = [-2, 5, 3, -8, 10, -1];
+const negativeSum = sumOfNegativeNumbers(numbers);
+console.log(negativeSum); // Output: -11
